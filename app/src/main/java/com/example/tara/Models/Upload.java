8db7d8ff2baf044effff769d724332d310a8db6d
kimatzu <mattzkie07@gmail.com>
2022-05-15 23:26:46 +0800

    Renamed UploadModel to Upload

--- app/src/main/java/com/example/tara/Models/Upload.java
+++ app/src/main/java/com/example/tara/Models/Upload.java
@@ -2,18 +2,18 @@ package com.example.tara.Models;
 
 import android.widget.TextView;
 
-public class UploadModel {
+public class Upload {
     public String address1,address2,city,postcode,province,year,brand,transmission,drivetrain,seats,type,fuelType,mileage,model,plateNumber
             ,insuranceUrl,priceRate,description,carUrl,bmy,location;
     public int vehicleCount;
 
-    public UploadModel(){
+    public Upload(){
 
     }
-    public UploadModel(String address1, String address2, String city, String postcode, String province,
-                       String year, String brand, String transmission, String drivetrain, String seats,
-                       String type, String fuelType, String mileage, String model, String plateNumber,
-                       String priceRate, String description, String carUrl,String bmy, String location, int vehicleCount) {
+    public Upload(String address1, String address2, String city, String postcode, String province,
+                  String year, String brand, String transmission, String drivetrain, String seats,
+                  String type, String fuelType, String mileage, String model, String plateNumber,
+                  String priceRate, String description, String carUrl, String bmy, String location, int vehicleCount) {
         this.address1 = address1;
         this.address2 = address2;
         this.city = city;
