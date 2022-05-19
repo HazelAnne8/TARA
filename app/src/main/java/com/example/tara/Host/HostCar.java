a0f4fcd2cd1a926d54407021f2308add29ff945c
kimatzu <mattzkie07@gmail.com>
2022-05-19 22:26:50 +0800

    fixed municipality adapter

--- app/src/main/java/com/example/tara/Host/HostCar.java
+++ app/src/main/java/com/example/tara/Host/HostCar.java
@@ -243,7 +243,7 @@ public class HostCar extends AppCompatActivity implements View.OnClickListener{
         ArrayAdapter<String> cityAdapter = new ArrayAdapter<>(this,R.layout.list_item,cityArr);
         ArrayAdapter<String> provinceAdapter = new ArrayAdapter<>(this,R.layout.list_item,provinceArr);
         ArrayAdapter<String> municipalityAdapter = new ArrayAdapter<>(this,R.layout.list_item,municipalityArr);
-        etMunicipality.setAdapter(cityAdapter);
+        etMunicipality.setAdapter(municipalityAdapter);
         etCity.setAdapter(cityAdapter);
         etProvince.setAdapter(provinceAdapter);
         userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
